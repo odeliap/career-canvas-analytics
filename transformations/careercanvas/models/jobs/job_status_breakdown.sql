40178f99eeb9f3976395044a34bf73a6107bf127
Odelia Putterman <odeliaputterman@gmail.com>
2023-08-05 20:47:27 -0500

    fix error

--- transformations/careercanvas/models/jobs/job_status_breakdown.sql
+++ transformations/careercanvas/models/jobs/job_status_breakdown.sql
@@ -11,4 +11,4 @@ status,
 count(*) * 100.0 / (select count(*) from job_statuses where "user_id"={{ var("user_id") }}) as percentage
 from job_statuses
 where "user_id"={{ var("user_id") }}
-group by status
+group by user_id, status
